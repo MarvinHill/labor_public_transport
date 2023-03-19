43667b70a2d2f998a60ae7aead1d532b803fedc9
MarvinHill <86198682+MarvinHill@users.noreply.github.com>
2023-03-19 16:16:55 +0100

    Spring Boot und Front End init

---
+++ SpringWebApplication/buga23publictransport/src/main/java/de/hhn/se/labswp/buga23publictransport/Get2bugaApplication.java
@@ -0,0 +1,13 @@
+package de.hhn.se.labswp.buga23publictransport;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Get2bugaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Get2bugaApplication.class, args);
+	}
+
+}
---
+++ SpringWebApplication/buga23publictransport/src/test/java/de/hhn/se/labswp/buga23publictransport/Get2bugaApplicationTests.java
@@ -0,0 +1,13 @@
+package de.hhn.se.labswp.buga23publictransport;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Get2bugaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
