c21da1a302063c59db988d5730f2bae04224fe0a
MarvinHill <86198682+MarvinHill@users.noreply.github.com>
2023-03-24 14:55:16 +0100

    changed app component and added new components

--- FrontEnd/src/app/app.module.ts
+++ FrontEnd/src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { TransportEntryComponent } from './transport-entry/transport-entry.component';
+import { RoutingTopBarComponent } from './routing-top-bar/routing-top-bar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TransportEntryComponent,
+    RoutingTopBarComponent
   ],
   imports: [
     BrowserModule,
---
+++ FrontEnd/src/app/transport-entry/transport-entry.component.ts
@@ -0,0 +1,12 @@
+import { Component, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-transport-entry',
+  templateUrl: './transport-entry.component.html',
+  styleUrls: ['./transport-entry.component.css']
+})
+export class TransportEntryComponent{
+
+  @Input() text: string = "no name";
+
+}
