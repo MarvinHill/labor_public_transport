81af76aa8752e9ec412f6692468b4e0baa6e4804
MarvinHill <86198682+MarvinHill@users.noreply.github.com>
2023-03-25 15:59:01 +0100

    simplified

--- SpringWebApplication/buga23publictransport/src/main/java/de/hhn/se/labswp/buga23publictransport/persistence/PublicTransportLineRepo.java
+++ SpringWebApplication/buga23publictransport/src/main/java/de/hhn/se/labswp/buga23publictransport/persistence/PublicTransportLineRepo.java
@@ -2,6 +2,4 @@ package de.hhn.se.labswp.buga23publictransport.persistence;
 
 import org.springframework.data.repository.CrudRepository;
 
-public interface PublicTransportLineRepo extends CrudRepository<PublicTransportLine, Long> {
-    Iterable<PublicTransportLine> findAll();
-}
+public interface PublicTransportLineRepo extends CrudRepository<PublicTransportLine, Long> { }
