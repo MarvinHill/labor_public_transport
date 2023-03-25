a8136d83a6b979c24a56a3bef50c768cb10e9dde
MarvinHill <86198682+MarvinHill@users.noreply.github.com>
2023-03-25 17:14:58 +0100

    added setter to LineScheduleEntry

--- SpringWebApplication/buga23publictransport/src/main/java/de/hhn/se/labswp/buga23publictransport/persistence/LineScheduleEntry.java
+++ SpringWebApplication/buga23publictransport/src/main/java/de/hhn/se/labswp/buga23publictransport/persistence/LineScheduleEntry.java
@@ -9,6 +9,42 @@ import java.time.LocalTime;
 
 @Entity
 public class LineScheduleEntry {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    private LocalTime arrivalTime;
+    private int waitTime;
+    private int delay;
+    private String stationDesignator;
+
+    protected LineScheduleEntry() {}
+
+    public LineScheduleEntry(LocalTime arrivalTime, int waitTime, int delay, String stationDesignator) {
+        this.arrivalTime = arrivalTime;
+        this.waitTime = waitTime;
+        this.delay = delay;
+        this.stationDesignator = stationDesignator;
+    }
+
+
+
+    public void setArrivalTime(LocalTime arrivalTime) {
+        this.arrivalTime = arrivalTime;
+    }
+
+    public void setWaitTime(int waitTime) {
+        this.waitTime = waitTime;
+    }
+
+    public void setDelay(int delay) {
+        this.delay = delay;
+    }
+
+    public void setStationDesignator(String stationDesignator) {
+        this.stationDesignator = stationDesignator;
+    }
+
     public Long getId() {
         return id;
     }
@@ -27,22 +63,5 @@ public class LineScheduleEntry {
 
     public String getStationDesignator() {
         return stationDesignator;
-    }
-
-    @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
-    private Long id;
-    private LocalTime arrivalTime;
-    private int waitTime;
-    private int delay;
-    private String stationDesignator;
-
-    protected LineScheduleEntry() {}
-
-    public LineScheduleEntry(LocalTime arrivalTime, int waitTime, int delay, String stationDesignator) {
-        this.arrivalTime = arrivalTime;
-        this.waitTime = waitTime;
-        this.delay = delay;
-        this.stationDesignator = stationDesignator;
     }
 }
