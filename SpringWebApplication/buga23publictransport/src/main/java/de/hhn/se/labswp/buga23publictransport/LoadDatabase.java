1710979ace2dd7ae846899e634557812a9444f2c
Kevin Reifschneider <Reifschneider.Kevin@gmx.de>
2023-03-28 16:48:54 +0200

    added real names for entries in the shuttle line.

--- SpringWebApplication/buga23publictransport/src/main/java/de/hhn/se/labswp/buga23publictransport/LoadDatabase.java
+++ SpringWebApplication/buga23publictransport/src/main/java/de/hhn/se/labswp/buga23publictransport/LoadDatabase.java
@@ -4,13 +4,16 @@ import de.hhn.se.labswp.buga23publictransport.persistence.LineScheduleEntry;
 import de.hhn.se.labswp.buga23publictransport.persistence.LineScheduleEntryRepo;
 import de.hhn.se.labswp.buga23publictransport.persistence.PublicTransportLine;
 import de.hhn.se.labswp.buga23publictransport.persistence.PublicTransportLineRepo;
+
 import java.time.LocalTime;
+
 import org.slf4j.LoggerFactory;
 import org.slf4j.Logger;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import javax.sound.sampled.Line;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -22,16 +25,34 @@ public class LoadDatabase {
     CommandLineRunner initDatabase(PublicTransportLineRepo lineRepo, LineScheduleEntryRepo entryRepo) {
         return args -> {
             log.info("Preloading Database Entries");
-            for (int i = 0; i < 5; i++) {
-                ArrayList<LineScheduleEntry> children = new ArrayList<>();
-                for (int j = 0; j < 5; j++) {
-                    LineScheduleEntry entry = new LineScheduleEntry(LocalTime.now(), 1, 0, "TestStation" + i + "," + j);
-                    children.add(entry);
-                    //log.info("Preloading " +  entryRepo.save(entry));
-                }
-                PublicTransportLine publicTransportLine = new PublicTransportLine("TestLine" + i, false, children);
-                log.info("Preloading " +  lineRepo.save(publicTransportLine));
-            }
+            // for (int i = 0; i < 5; i++) {
+            //     ArrayList<LineScheduleEntry> children = new ArrayList<>();
+            //     for (int j = 0; j < 5; j++) {
+            //         LineScheduleEntry entry = new LineScheduleEntry(LocalTime.now(), 1, 0, "TestStation" + i + "," + j);
+            //         children.add(entry);
+            //         //log.info("Preloading " +  entryRepo.save(entry));
+            //     }
+            //     PublicTransportLine publicTransportLine = new PublicTransportLine("TestLine" + i, false, children);
+            //     log.info("Preloading " +  lineRepo.save(publicTransportLine));
+            // }
+
+
+            var entries = new ArrayList<LineScheduleEntry>();
+            entries.add(new LineScheduleEntry(
+                    LocalTime.of(8, 30), 0, 0, "SAP Arena"));
+            entries.add(new LineScheduleEntry(
+                    LocalTime.of(8, 30), 0, 0, "SAP Arena S-Bf\n" +
+                    "(MA-Arena/Maimarkt)"));
+            entries.add(new LineScheduleEntry(
+                    LocalTime.of(8, 30), 0, 0, "BUGA 23 Haupteingang\n" +
+                    "Spinelli-Park "));
+            entries.add(new LineScheduleEntry(
+                    LocalTime.of(8, 30), 0, 0, "Neuostheim"));
+            entries.add(new LineScheduleEntry(
+                    LocalTime.of(8, 30), 0, 0, "    Luisenpark/Technoseum\n" +
+                    "(BUGA23)"));
+            var ptl = new PublicTransportLine("SAP Arena S-Bf", false, entries);
+            log.info("Preloading " + lineRepo.save(ptl));
         };
     }
 
