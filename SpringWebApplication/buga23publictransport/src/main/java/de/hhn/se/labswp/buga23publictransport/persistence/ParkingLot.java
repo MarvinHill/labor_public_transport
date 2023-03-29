ede7e6203338b15635f9c7fabbaf411814f5bb0a
Nedim <nlimani@Stud.hs-heilbronn.de>
2023-03-29 13:09:08 +0200

    changed entity class for parking lot and its repository

---
+++ SpringWebApplication/buga23publictransport/src/main/java/de/hhn/se/labswp/buga23publictransport/persistence/ParkingLot.java
@@ -0,0 +1,121 @@
+package de.hhn.se.labswp.buga23publictransport.persistence;
+
+import de.hhn.se.labswp.buga23publictransport.business.AccessRight;
+import de.hhn.se.labswp.buga23publictransport.business.ParkingStatus;
+import de.hhn.se.labswp.buga23publictransport.business.ParkingType;
+import jakarta.persistence.*;
+import com.vividsolutions.jts.geom.Polygon;
+
+@Entity
+public class ParkingLot {
+
+    @Id
+    @GeneratedValue
+    Long id;
+    @Convert(converter = PolygonConverter.class)
+    @Column(length = 1000)
+    @Lob
+    Polygon area;
+    Double longtitude, latitude;
+    String name;
+    boolean barrierfree;
+    AccessRight accessRight;    // 1 = employee, 2 = visitor
+    boolean charging;
+    ParkingStatus parkingStatus;  // 1 = available, 2 = full, 3 = closed
+    ParkingType parkingType;    // 1 = bike, 2 = car, 3 = bus
+
+    public ParkingLot(Polygon area, Double longtitude, Double latitude, String name, boolean barrierfree, AccessRight accessRight, boolean charging, ParkingStatus parkingStatus, ParkingType parkingType) {
+        this.area = area;
+        this.longtitude = longtitude;
+        this.latitude = latitude;
+        this.name = name;
+        this.barrierfree = barrierfree;
+        this.accessRight = accessRight;
+        this.charging = charging;
+        this.parkingStatus = parkingStatus;
+        this.parkingType = parkingType;
+    }
+
+    public ParkingLot() { }
+
+    public void setID(Long id) {
+        this.id = id;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setArea(Polygon area) {
+        this.area = area;
+    }
+
+    public Polygon getArea() {
+        return area;
+    }
+
+    public void setLongtitude(Double longtitude) {
+        this.longtitude = longtitude;
+    }
+
+    public Double getLongtitude() {
+        return longtitude;
+    }
+
+    public void setLatitude(Double latitude) {
+        this.latitude = latitude;
+    }
+
+    public Double getLatitude() {
+        return latitude;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setBarrierfree(boolean barrierfree) {
+        this.barrierfree = barrierfree;
+    }
+
+    public boolean getBarrierfree() {
+        return barrierfree;
+    }
+
+    public void setAccessRight(AccessRight accessRight) {
+        this.accessRight = accessRight;
+    }
+
+    public AccessRight getAccessRight() {
+        return accessRight;
+    }
+
+    public void setCharging(boolean charging) {
+        this.charging = charging;
+    }
+
+    public boolean getCharging() {
+        return charging;
+    }
+
+    public void setParkingStatus(ParkingStatus parkingStatus) {
+        this.parkingStatus = parkingStatus;
+    }
+
+    public ParkingStatus getParkingStatus() {
+        return parkingStatus;
+    }
+
+    public void setParkingType(ParkingType parkingType) {
+        this.parkingType = parkingType;
+    }
+
+    public ParkingType getParkingType() {
+        return parkingType;
+    }
+
+}
---
+++ SpringWebApplication/buga23publictransport/src/main/java/de/hhn/se/labswp/buga23publictransport/persistence/ParkingLotRepository.java
@@ -0,0 +1,5 @@
+package de.hhn.se.labswp.buga23publictransport.persistence;
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface ParkingLotRepository  extends CrudRepository<ParkingLot, Long> { }
