fcf09a1e75664334d1301db56753b814c1de13b2
JonasGierlich <jonasgierlich1@gmail.com>
2023-03-29 12:18:40 +0200

    added routing to map (currently through parkingLot)

--- FrontEnd/src/app/app-routing.module.ts
+++ FrontEnd/src/app/app-routing.module.ts
@@ -2,11 +2,13 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import {MainViewComponent} from './main-view/main-view.component'
 import { ShuttleViewComponent } from './shuttle-view/shuttle-view.component';
+import {MapComponent} from "./map/map.component";
 
 const routes: Routes = [
   { path: '', redirectTo: '/main', pathMatch: 'full' },
   {path:'main', component: MainViewComponent},
   {path:'shuttle', component: ShuttleViewComponent},
+  {path:'map', component: MapComponent}
 ];
 
 @NgModule({
