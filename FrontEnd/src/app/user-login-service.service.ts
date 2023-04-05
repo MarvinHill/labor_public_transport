b4b8e734d1d59bdcae64463bdbe213a8ef98dc71
MarvinHill <86198682+MarvinHill@users.noreply.github.com>
2023-04-05 23:11:42 +0200

    added methode that opens login screen

--- FrontEnd/src/app/user-login-service.service.ts
+++ FrontEnd/src/app/user-login-service.service.ts
@@ -1,4 +1,5 @@
 import { Injectable } from '@angular/core';
+import { Router } from '@angular/router';
 import { Subject } from 'rxjs';
 
 @Injectable({
@@ -8,14 +9,20 @@ export class UserLoginServiceService {
 
   isLoggedIn: Subject<boolean> = new Subject<boolean>(); 
   logginStatus: boolean = false;
+  router: Router;
 
-  constructor(){
+  constructor(router : Router){
     this.isLoggedIn.next(this.logginStatus);
+    this.router = router;
   }
 
   changeValue(){
     this.logginStatus = !this.logginStatus;
     this.isLoggedIn.next(this.logginStatus)
+  }
+
+  showLoginPage(){
+    this.router.navigateByUrl('/second')
   }
 
 }
