04fef2328e03fc7bede7b46f082ad98c47ffb7ef
Kevin Reifschneider <Reifschneider.Kevin@gmx.de>
2023-04-05 18:50:51 +0200

    added shuttle line service

---
+++ FrontEnd/src/app/services/shuttle-line.service.ts
@@ -0,0 +1,19 @@
+import { Injectable } from '@angular/core';
+import { ShuttleLine } from '../ShuttleLine';
+import { Observable } from 'rxjs';
+import { HttpClient } from '@angular/common/http';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ShuttleLineService {
+  private http: HttpClient;
+
+  constructor(http: HttpClient) {
+    this.http = http;
+  }
+
+  public getShuttleLines(): Observable<ShuttleLine[]> {
+      return this.http.get<ShuttleLine[]>('http://localhost:8080/ptl');
+  }
+}
