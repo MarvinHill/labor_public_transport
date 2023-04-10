adb81f78f8205e187be1628e459a8dcafbd1c5a8
jfreudenbe <119427964+jfreudenbe@users.noreply.github.com>
2023-04-10 13:49:48 +0200

    implemented buttons for carousel

--- FrontEnd/src/app/main-view/main-view.component.ts
+++ FrontEnd/src/app/main-view/main-view.component.ts
@@ -1,12 +1,59 @@
-import { Component } from '@angular/core';
+import { Component, AfterViewInit } from '@angular/core';
 
 @Component({
   selector: 'app-main-view',
   templateUrl: './main-view.component.html',
   styleUrls: ['./main-view.component.css']
 })
-export class MainViewComponent {
+export class MainViewComponent implements AfterViewInit {
+
+  ngAfterViewInit() {
+    const track = document.querySelector('.carousel__track') as HTMLElement;
+    const slides = Array.from(track.children) as HTMLElement[];
+    const nextButton = document.querySelector('.carousel__button--right');
+    const prevButton = document.querySelector('.carousel__button--left');
+    const dotsNav = document.querySelector('.carousel__nav');
+    const dots = Array.from(dotsNav.children);
+
+    const slideWidth = slides[0].getBoundingClientRect().width;
+    
+    const setSlidePosition = (slide, index) => {
+      slide.style.left = slideWidth * index + 'px';
+    };
+    slides.forEach(setSlidePosition);
+
+    const moveToSlide = (track, currentSlide, targetSlide) => {
+      track.style.transform = 'translateX(-' + targetSlide.style.left +  ')';
+      currentSlide.classList.remove('current-slide');
+      targetSlide.classList.add('current-slide')
+    }
+
+  
+
+    nextButton.addEventListener('click', e => {
+      const currentSlide = track.querySelector('.current-slide') as HTMLElement;
+      const nextSlide = currentSlide.nextElementSibling as HTMLElement;
+      
+      moveToSlide(track, currentSlide, nextSlide)
+    })
+
+    prevButton.addEventListener('click', e => {
+      const currentSlide = track.querySelector('.current-slide') as HTMLElement;
+      const prevSlide = currentSlide.previousElementSibling as HTMLElement;
+
+      moveToSlide(track, currentSlide, prevSlide);
+    })
+
+   
+
+
+
+  }
 
 }
 
-console.log('te123st')
+
+
+
+
+
