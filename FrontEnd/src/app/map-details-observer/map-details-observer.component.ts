277f8b10dea382212f49adc51ae15e8bba745869
Marvin Hill <86198682+MarvinHill@users.noreply.github.com>
2023-04-12 16:10:10 +0200

    created data classes

--- FrontEnd/src/app/map-details-observer/map-details-observer.component.ts
+++ FrontEnd/src/app/map-details-observer/map-details-observer.component.ts
@@ -1,5 +1,8 @@
 import { Component } from '@angular/core';
 import { DataServiceService } from '../data-service.service';
+import { ShuttleLine } from '../ShuttleLine';
+import { ParkingLot } from '../ParkingLot';
+import { RnvLine } from '../RnvLine';
 
 @Component({
   selector: 'app-map-details-observer',
@@ -8,12 +11,27 @@ import { DataServiceService } from '../data-service.service';
 })
 export class MapDetailsObserverComponent {
 
-  state : number = 1; // 0 Nothing, 1 Parking, 2 RNV
+  state : number = 0; // 0 Nothing, 1 Parking, 2 RNV, 3 Shuttle Line
+  data = null;
 
   constructor(private service : DataServiceService){}
 
   openMaps(){
     this.service.openMapExternal('Asperg', 'Ludwigsburg');
+  }
+
+  changeDisplay(data){
+    switch(data.constructor){
+    case(ShuttleLine): this.state = 3; break;
+    case(RnvLine): this.state = 2; break;
+    case(ParkingLot): this.state = 1; break;
+    default: this.state = 0; break;
+    }
+    this.data = data;
+  }
+
+  hide(){
+    
   }
 
 }
