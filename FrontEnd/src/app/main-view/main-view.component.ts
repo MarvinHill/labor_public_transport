1dea46e33b1ae9dafdfa2ee91631ca88df18c9f0
jfreudenbe <119427964+jfreudenbe@users.noreply.github.com>
2023-04-12 02:20:15 +0200

    implemented nav bar and fixxed bugs

--- FrontEnd/src/app/main-view/main-view.component.ts
+++ FrontEnd/src/app/main-view/main-view.component.ts
@@ -1,4 +1,5 @@
 import { Component, AfterViewInit } from '@angular/core';
+import {Router} from '@angular/router';
 
 @Component({
   selector: 'app-main-view',
@@ -14,6 +15,7 @@ export class MainViewComponent implements AfterViewInit {
     const prevButton = document.querySelector('.carousel__button--left');
     const dotsNav = document.querySelector('.carousel__nav');
     const dots = Array.from(dotsNav.children);
+    const clickCurrentSlide = document.querySelector('.current-slide');
 
     const slideWidth = slides[0].getBoundingClientRect().width;
     
@@ -28,28 +30,66 @@ export class MainViewComponent implements AfterViewInit {
       targetSlide.classList.add('current-slide')
     }
 
-  
+    const updateDots = (currentDot, targetDot) => {
+      currentDot.classList.remove('current-slide');
+      targetDot.classList.add('current-slide');
+    }
+
+    const hideShowArrows = (slides, prevButton, nextButton, targetIndex) => {
+      if(targetIndex === 0){
+        prevButton.classList.add('is-hidden');
+        nextButton.classList.remove('is-hidden');
+      } else if (targetIndex === slides.length - 1){
+        prevButton.classList.remove('is-hidden');
+        nextButton.classList.add('is-hidden');
+      } else {
+        prevButton.classList.remove('is-hidden');
+        nextButton.classList.remove('is-hidden');
+      }
+    }
 
     nextButton.addEventListener('click', e => {
       const currentSlide = track.querySelector('.current-slide') as HTMLElement;
       const nextSlide = currentSlide.nextElementSibling as HTMLElement;
-      
+      const currentDot = dotsNav.querySelector('.current-slide') as HTMLElement;
+      const nextDot = currentDot.nextElementSibling;
+      const nextIndex = slides.findIndex(slide => slide === nextSlide);
       moveToSlide(track, currentSlide, nextSlide)
+      updateDots(currentDot, nextDot);
+      hideShowArrows(slides, prevButton, nextButton, nextIndex);
     })
 
     prevButton.addEventListener('click', e => {
       const currentSlide = track.querySelector('.current-slide') as HTMLElement;
       const prevSlide = currentSlide.previousElementSibling as HTMLElement;
-
+      const currentDot = dotsNav.querySelector('.current-slide') as HTMLElement;
+      const prevDot = currentDot.previousElementSibling;
+      const prevIndex = slides.findIndex(slide => slide === prevSlide);
       moveToSlide(track, currentSlide, prevSlide);
-    })
+      updateDots(currentDot, prevDot);
+      hideShowArrows(slides, prevButton, nextButton, prevIndex);
+    });
 
-   
+    dotsNav.addEventListener('click', (e: MouseEvent) => {
+      const targetDot = (e.target as Element).closest('button');
 
+      if(!targetDot) return;
 
+      const currentSlide = track.querySelector('.current-slide') as HTMLElement;
+      const currentDot = dotsNav.querySelector('.current-slide') as HTMLElement;
+      const targetIndex = dots.findIndex(dot => dot === targetDot);
+      const targetSlide = slides[targetIndex];
+
+      moveToSlide(track, currentSlide, targetSlide);
+      updateDots(currentDot, targetDot);
+      hideShowArrows(slides, prevButton, nextButton, targetIndex);
+     
+     
+    });
+    
+  
 
   }
-
 }
 
 
