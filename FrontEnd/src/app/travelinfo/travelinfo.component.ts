0b4511f36f8b1d4466fe043d7e5a1e0c9f103cee
jfreudenbe <119427964+jfreudenbe@users.noreply.github.com>
2023-05-12 14:11:36 +0200

    new info + new design

--- FrontEnd/src/app/travelinfo/travelinfo.component.ts
+++ FrontEnd/src/app/travelinfo/travelinfo.component.ts
@@ -6,5 +6,15 @@ import { Component } from '@angular/core';
   styleUrls: ['./travelinfo.component.css']
 })
 export class TravelinfoComponent {
+  redirectToParkOrdnung() {
+    window.location.href = 'https://www.buga23.de/parkordnung/';
+  }
 
+  redirectToTicketShop() {
+    window.location.href = 'https://tickets.buga23.de/shop/103';
+  }
+
+  redirectToBarrierefreiheit () {
+    window.location.href = 'https://www.buga23.de/besuch-service/barrierefreiheit-mobilitaetsverleih/';
+  }
 }
