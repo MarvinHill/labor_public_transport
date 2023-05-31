cd3b8757b761e5b9b535f1cfa36cebb4c86596cd
MarvinHill <86198682+MarvinHill@users.noreply.github.com>
2023-05-31 18:36:33 +0200

    improved on change detection of the search componen

--- FrontEnd/src/app/search-view/search-view.component.ts
+++ FrontEnd/src/app/search-view/search-view.component.ts
@@ -10,7 +10,30 @@ import { Searchable } from '../Searchable';
 export class SearchViewComponent{
 constructor(protected searchService : SearchService){}
 
+protected searchQuery : Promise<Searchable[]>;
 protected cachedSearchResults : Map<string, Searchable[]>;
 
 searchResults : Searchable[] ;
+
+protected textSearch(searchValue: string): void {
+  this.searchService.loading = true;
+  this.searchService.search(searchValue).then(value => {
+    this.cachedSearchResults =  this.returnWithType(value.flat());
+    
+    this.searchService.loading = false;
+  });
+}
+
+returnWithType(search : Searchable[]) : Map<string, Searchable[]> {
+  var searchResults : Map<string, Searchable[]> = new Map<string, Searchable[]>();
+  
+  search.forEach(element => {
+    if(!searchResults.has(element.category)){
+      searchResults.set(element.category, []);
+    }  
+    searchResults.get(element.category)?.push(element);
+  });
+
+  return searchResults;
+}
 }
