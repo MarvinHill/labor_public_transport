6779f757d8f49ab6c52e67df11425ff1dceb1be4
Nedim <nlimani@Stud.hs-heilbronn.de>
2023-06-21 11:52:57 +0200

    restored ÖPNV subpage

--- FrontEnd/src/app/app-routing.module.ts
+++ FrontEnd/src/app/app-routing.module.ts
@@ -15,6 +15,8 @@ import { PreisService } from './preis.service';
 import { ConnectionTicketComponent } from './connection-ticket/connection-ticket.component';
 import {BikeParkingItemComponent} from "./bike-parking-item/bike-parking-item.component";
 import {CampsiteParkingItemComponent} from "./campsite-parking-item/campsite-parking-item.component";
+import { PublicTransportViewComponent } from './public-transport-view/public-transport-view.component';
+
 
 const routes: Routes = [
   { path: '', redirectTo: '/main', pathMatch: 'full' },
@@ -29,7 +31,7 @@ const routes: Routes = [
   {path: 'connection-ticket', component: ConnectionTicketComponent},
   {path: 'PreisService', component: PreisService},
   {path:'impressum', component: ImpressumComponent},
-
+  {path:'public-transport', component: PublicTransportViewComponent},
   {path:'bike-parking', component: BikeParkingItemComponent},
   {path:'campsites', component: CampsiteParkingItemComponent}
 ];
